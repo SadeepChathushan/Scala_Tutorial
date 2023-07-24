15a6921e80276e73fc1b7a4e757c885f4bb14551
P S C Nalindika <121891368+SadeepChathushan@users.noreply.github.com>
2023-07-24 20:49:24 +0530

    Add files via upload

---
+++ Scala_Tutorial_6/Q1.scala
@@ -0,0 +1,18 @@
+object Q1 {
+  val alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+  def Enc(c: Char, key: Int, a: String) = a((a.indexOf(c.toUpper) + key) % a.size)
+
+  def Dec(c: Char, key: Int, a: String) = a((a.indexOf(c.toUpper) - key) % a.size)
+
+  def cipher(algo: (Char, Int, String) => Char, s: String, key: Int, a: String) = s.map(algo(_, key, a))
+
+  def main(args: Array[String]) = {
+    val encword = cipher(Enc, "BAT", 3, alphabet)
+    println(encword)
+    val decword = cipher(Dec, encword, 3, alphabet)
+    println(decword)
+
+  }
+
+}
